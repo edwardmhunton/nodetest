e2d2a3524042f82f5814fd14de6191bedaed2b85
Ed Hunton <ehunton@linius.com>
2020-02-10 15:36:59 +0000

    more bs

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Linius from Fargate!!!!!!@Arrag, still not working 5');
+  res.send('Hello Linius from Fargate!!!!!!@Arrag, still not working 6');
 });
 
 app.listen(PORT, HOST);
